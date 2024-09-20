0b3d9a2a9d34b9cc704ec9a341bc92bb5cbf694c
KTDKasp <churroskasp@gmail.com>
2024-09-20 09:04:29 +0300

    Add suspense in root page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,60 +1,66 @@
 import {
-	Container,
-	Filters,
-	ProductsCardList,
-	Title,
-	TopBar,
+  Container,
+  Filters,
+  ProductsCardList,
+  Title,
+  TopBar,
 } from '@/components/shared';
 import { prisma } from '@/prisma/prisma-client';
+import { Suspense } from 'react';
 
 export default async function Home() {
-	const categories = await prisma.category.findMany({
-		include: {
-			products: {
-				include: {
-					ingredients: true,
-					variants: true,
-				}
-			}
-		}
-	})
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          variants: true,
+        },
+      },
+    },
+  });
 
-	return (
-		<>
-			<Container>
-				<Title text="Все пиццы" size="lg" className="font-extrabold" />
-			</Container>
+  return (
+    <>
+      <Container>
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
 
-			<TopBar categories={categories.filter(category => category.products.length > 0)}/>
+      <TopBar
+        categories={categories.filter(
+          (category) => category.products.length > 0
+        )}
+      />
 
-			<Container className="pb-14">
-				<div className="flex gap-12">
-					{/* Filters */}
-					<div className="w-[245px]">
-						<Filters />
-					</div>
+      <Container className="pb-14">
+        <div className="flex gap-12">
+          {/* Filters */}
+          <div className="w-[245px]">
+            <Suspense>
+              <Filters />
+            </Suspense>
+          </div>
 
-					{/* Pizzas */}
-					<div className="flex-1">
-						<div className="flex flex-col gap-[50px]">
-							{
-								categories.map((category) => (
-									category.products.length > 0 && (
-										<ProductsCardList 
-											key={category.id}
-											categoryId={category.id}
-											title={category.name}
-											productItems={category.products}
-										/>
-									)
-								))
-							}
-						</div>
-					</div>
-				</div>
-			</Container>
-		</>
-	);
+          {/* Pizzas */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-[50px]">
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsCardList
+                      key={category.id}
+                      categoryId={category.id}
+                      title={category.name}
+                      productItems={category.products}
+                    />
+                  )
+              )}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
 
 // # TODO: Глобально исправить добавление товара в корзину и его рендер в корзине, 10:32:00
